5373f16aed6867670c50358ca9445a2f1f17d386
Ryan Linton <ryanlntn@gmail.com>
2014-08-14 16:36:08 -0700

    Tighten up style

--- lib/ProMotion/menu/delegate.rb
+++ lib/ProMotion/menu/delegate.rb
@@ -1,6 +1,7 @@
 module ProMotion
   module Menu
     module Delegate
+
       def menu=(m)
         @menu = m
       end
@@ -12,16 +13,17 @@ module ProMotion
       alias_method :slide_menu, :menu
 
       def has_menu?
-        !slide_menu.nil?
+        !menu.nil?
       end
       alias_method :has_slide_menu?, :has_menu?
 
       def open_menu(content, options={})
-        self.slide_menu = ProMotion::Menu::Drawer.new(content, options)
-        open_root_screen slide_menu
-        slide_menu
+        self.menu = ProMotion::Menu::Drawer.new(content, options)
+        open_root_screen menu
+        menu
       end
       alias_method :open_slide_menu, :open_menu # backwards compat
+
     end
   end
 end
--- lib/ProMotion/menu/drawer.rb
+++ lib/ProMotion/menu/drawer.rb
@@ -3,18 +3,12 @@ module ProMotion
     class Drawer < MMDrawerController
       include ::ProMotion::ScreenModule
 
-      #
-      # ProMotion::Menu
-      #
-      # This is added as the root view controller when using the `open_slide_menu` method in your application delegate.
-      #
-
-      def self.new(content, options={})
+      def self.new(center, options={})
         left_vc = options.fetch(:left, nil)
         right_vc = options.fetch(:right, nil)
 
         menu = alloc.init
-        menu.content_controller = content unless content.nil?
+        menu.center_controller = center unless center.nil?
         menu.left_controller = left_vc if left_vc
         menu.right_controller = right_vc if right_vc
 
@@ -28,15 +22,15 @@ module ProMotion
         self.show_right(animated) if side == :right
       end
 
-      def show_left(animated = true)
+      def show_left(animated=true)
         openDrawerSide MMDrawerSideLeft, animated: animated, completion: default_completion_block
       end
 
-      def show_right(animated = true)
+      def show_right(animated=true)
         openDrawerSide MMDrawerSideRight, animated: animated, completion: default_completion_block
       end
 
-      def hide(animated = true)
+      def hide(animated=true)
         closeDrawerAnimated animated, completion: default_completion_block
       end
 
@@ -56,18 +50,21 @@ module ProMotion
         self.rightDrawerViewController
       end
 
-      def content_controller=(c)
+      def center_controller=(c)
         self.centerViewController = prepare_controller_for_pm(c)
       end
+      alias_method :content_controller=, :center_controller=
 
-      def content_controller
+      def center_controller
         self.centerViewController
       end
+      alias_method :content_controller, :center_controller
 
       def controller=(side={})
         self.left_controller = side[:left] if side[:left]
         self.right_controller = side[:right] if side[:right]
-        self.content_controller = side[:content] if side[:content]
+        self.center_controller = side[:center] if side[:center]
+        self.center_controller ||= side[:content] if side[:content]
       end
 
       alias_method :controllers=, :controller=
@@ -75,10 +72,10 @@ module ProMotion
       def controller(side)
         self.left_controller if side == :left
         self.right_controller if side == :right
-        self.content_controller if side == :content
+        self.center_controller if side == :content || side == :center
       end
 
-      protected
+    protected
 
       def prepare_controller_for_pm(controller)
         controller = set_up_screen_for_open(controller, {})
@@ -89,6 +86,7 @@ module ProMotion
       def default_completion_block
         -> (completed) { true }
       end
+
     end
   end
 end
