3ac764620708944852714c64c26a45f812e37ea6
Matt Brewer <matt.brewer@me.com>
2014-08-27 15:38:34 -0400

    Refactored specs to match modules

--- spec/drawer_spec.rb
+++ spec/drawer_spec.rb
@@ -24,24 +24,6 @@ describe ProMotion::Menu::Drawer do
     menu.center_controller.should.be.instance_of BlankScreen
   end
 
-  it "presents the menu controller when requested" do
-    menu = ProMotion::Menu::Drawer.new @content, left: @left
-    menu.show(:left)
-    wait(0.5) { menu.openSide.should == MMDrawerSideLeft }
-  end
-
-  it "presents the menu controller without animation when requested" do
-    menu = ProMotion::Menu::Drawer.new @content, left: @left
-    menu.show(:left, false)
-    wait(0.1) { menu.openSide.should == MMDrawerSideLeft }
-  end
-
-  it "presents the content controller when requested" do
-    menu = ProMotion::Menu::Drawer.new @content, left: @left
-    menu.hide
-    wait(0.5) { menu.openSide.should == MMDrawerSideNone }
-  end
-
   it "lets me wrap the content controller in a UINavigationController" do
     center_controller = BlankScreen.new(nav_bar: true)
     menu = ProMotion::Menu::Drawer.new center_controller, left: @left
@@ -65,59 +47,6 @@ describe ProMotion::Menu::Drawer do
     menu.left_controller.should == @left
     menu.right_controller.should == @right
     menu.center_controller.should == @content
-  end
-
-  describe "#toggle" do
-    before do
-      @menu = ProMotion::Menu::Drawer.new @content, left: @left, right: @right
-    end
-
-    describe "from left" do
-
-      it "opens the menu " do
-        @menu.toggle(:left)
-        wait 0.25 do
-          @menu.openSide.should == MMDrawerSideLeft
-        end
-      end
-
-      it "closes the menu" do
-        @menu.show(:left)
-        wait 0.25 do
-
-          @menu.openSide.should == MMDrawerSideLeft
-          @menu.toggle(:left)
-
-          wait 0.25 do
-            @menu.openSide.should == MMDrawerSideNone
-          end
-        end
-      end
-    end
-
-    describe "from right" do
-      
-      it "opens the menu" do
-        @menu.toggle(:right)
-        wait 0.25 do
-          @menu.openSide.should == MMDrawerSideRight
-        end
-      end
-
-      it "closes the menu" do
-        @menu.show(:right)
-        wait 0.25 do
-
-          @menu.openSide.should == MMDrawerSideRight
-          @menu.toggle(:right)
-
-          wait 0.25 do
-            @menu.openSide.should == MMDrawerSideNone
-          end
-        end
-      end
-
-    end
   end
 
 end
---
+++ spec/visibility_spec.rb
@@ -0,0 +1,82 @@
+describe ProMotion::Menu::Visibility do
+
+  before do
+    @delegate = UIApplication.sharedApplication.delegate
+    @left = LeftNavScreen.new
+    @right = RightNavScreen.new
+    @content = BlankScreen.new
+  end
+
+  describe "#show" do
+    it "presents the menu controller when requested" do
+      menu = ProMotion::Menu::Drawer.new @content, left: @left
+      menu.show(:left)
+      wait(0.5) { menu.openSide.should == MMDrawerSideLeft }
+    end
+
+    it "presents the menu controller without animation when requested" do
+      menu = ProMotion::Menu::Drawer.new @content, left: @left
+      menu.show(:left, false)
+      wait(0.1) { menu.openSide.should == MMDrawerSideLeft }
+    end
+  end
+
+  describe "#hide" do
+    it "presents the content controller when requested" do
+      menu = ProMotion::Menu::Drawer.new @content, left: @left
+      menu.hide
+      wait(0.5) { menu.openSide.should == MMDrawerSideNone }
+    end
+  end
+
+  describe "#toggle" do
+    before do
+      @menu = ProMotion::Menu::Drawer.new @content, left: @left, right: @right
+    end
+
+    describe "from left" do
+      it "opens the menu " do
+        @menu.toggle(:left)
+        wait 0.25 do
+          @menu.openSide.should == MMDrawerSideLeft
+        end
+      end
+
+      it "closes the menu" do
+        @menu.show(:left)
+        wait 0.25 do
+
+          @menu.openSide.should == MMDrawerSideLeft
+          @menu.toggle(:left)
+
+          wait 0.25 do
+            @menu.openSide.should == MMDrawerSideNone
+          end
+        end
+      end
+    end
+
+    describe "from right" do
+      it "opens the menu" do
+        @menu.toggle(:right)
+        wait 0.25 do
+          @menu.openSide.should == MMDrawerSideRight
+        end
+      end
+
+      it "closes the menu" do
+        @menu.show(:right)
+        wait 0.25 do
+
+          @menu.openSide.should == MMDrawerSideRight
+          @menu.toggle(:right)
+
+          wait 0.25 do
+            @menu.openSide.should == MMDrawerSideNone
+          end
+        end
+      end
+    end
+  end
+
+end
