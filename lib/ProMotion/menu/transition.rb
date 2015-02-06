bf0b55d41a37c93f0332a913e53284bfd3c589fc
bfegan@gmail.com <BrianEgan@outlook.com>
2015-02-05 23:37:20 -0500

    Simplified VISUAL_STATES hash, more snake case
    - Flattened VISUAL_STATES hash to remove nested “Description” (moved to
    a commented block below the hash).
    - Changed ‘visualBlock’ from camel case to snake case.

--- lib/ProMotion/menu/transition.rb
+++ lib/ProMotion/menu/transition.rb
@@ -2,42 +2,31 @@ module ProMotion; module Menu
   module Transition
 
      VISUAL_STATES = {
-      slide_and_scale:
-        {
-          block: MMDrawerVisualState.slideAndScaleVisualStateBlock,
-          # "Creates a slide and scale visual state block that gives an experience similar to Mailbox.app. It scales from 90% to 100%, and translates 50 pixels in the x direction. In addition, it also sets alpha from 0.0 to 1.0."
-        },
-      slide:
-        {
-          block: MMDrawerVisualState.slideVisualStateBlock,
-          # "Creates a slide visual state block that gives the user an experience that slides at the same speed of the center view controller during animation. This is equal to calling parallaxVisualStateBlockWithParallaxFactor: with a parallax factor of 1.0."
-        },
-      parallax:
-        {
-          block: MMDrawerVisualState.parallaxVisualStateBlockWithParallaxFactor(3),
-          # "Creates a parallax experience that slides the side drawer view controller at a different rate than the center view controller during animation. For every parallaxFactor of points moved by the center view controller, the side drawer view controller will move 1 point. Passing in 1.0 is the equivalent of a applying a sliding animation, while passing in 'MAX_FLOAT' is the equivalent of having no animation at all."
-        },
-      swinging_door:
-        {
-          block: MMDrawerVisualState.swingingDoorVisualStateBlock,
-          # "Creates a swinging door visual state block that gives the user an experience that animates the drawer in along the hinge."
-        }
+      slide_and_scale: MMDrawerVisualState.slideAndScaleVisualStateBlock,
+      slide: MMDrawerVisualState.slideVisualStateBlock,
+      parallax: MMDrawerVisualState.parallaxVisualStateBlockWithParallaxFactor(3),
+      swinging_door: MMDrawerVisualState.swingingDoorVisualStateBlock
     }
 
-    def transition_animation=(visualBlock)
-      # Parallax requires a parallaxFactor. Set it by passing the visualBlock block in with underscore parallaxFactor attached to the end. i.e. transition_animation = :parallax_6 for parallaxFactor 6.
+    # slide_and_scale: Creates a slide visual state block that gives the user an experience that slides at the same speed of the center view controller during animation. This is equal to calling parallaxVisualStateBlockWithParallaxFactor: with a parallax factor of 1.0. This is similar to the menu in Spotify's app.
+    # parallax: Creates a parallax experience that slides the side drawer view controller at a different rate than the center view controller during animation. For every parallaxFactor of points moved by the center view controller, the side drawer view controller will move 1 point. Passing in 1.0 is the equivalent of a applying a sliding animation, while passing in 'MAX_FLOAT' is the equivalent of having no animation at all.
+        # parallaxFactor is fed in by appending an underscore and the factor to 'parallax', i.e. :parallax_6 for a desired parallax factor of 6
+    # swinging_door: Creates a swinging door visual state block that gives the user an experience that animates the drawer in along the hinge.
+    # slide_and_scale: Creates a slide and scale visual state block that gives an experience similar to Mailbox.app. It scales from 90% to 100%, and translates 50 pixels in the x direction. In addition, it also sets alpha from 0.0 to 1.0.
 
-      self.setDrawerVisualStateBlock(mask_for_transition(visualBlock))
+    def transition_animation=(visual_block)
+      # Parallax requires a parallax_factor. Set it by passing the visual_block block in with underscore parallax_factor attached to the end. i.e. transition_animation = :parallax_6 for parallax_factor 6.
+      self.setDrawerVisualStateBlock(mask_for_transition(visual_block))
     end
 
-    def mask_for_transition(visualBlock)
-      unless visualBlock.nil?
-        if visualBlock.include? "parallax"
-          parallax_factor = visualBlock.include?("_") ? visualBlock.split("_")[1].to_i : 3
+    def mask_for_transition(visual_block)
+      unless visual_block.nil?
+        if visual_block.include? "parallax"
+          parallax_factor = visual_block.include?("_") ? visual_block.split("_")[1].to_i : 3
           visual_state = MMDrawerVisualState.parallaxVisualStateBlockWithParallaxFactor(parallax_factor)
         end
       end
-      visual_state ||= VISUAL_STATES.keys.include?(visualBlock) ? VISUAL_STATES[visualBlock][:block] : nil
+      visual_state ||= VISUAL_STATES.keys.include?(visual_block) ? VISUAL_STATES[visual_block] : nil
       visual_state
     end
 
